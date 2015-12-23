4b1522c89bcba8e69b13fabec4beb19d0c6b037d
bexben <benpilot27@yahoo.com>
2015-12-23 14:37:06 -0500

    dice

---
+++ Programs/Dice.py
@@ -0,0 +1,15 @@
+import random
+
+def dice():
+    total = 0
+    for x in range(1,1000):
+        num = random.randrange(1,7)
+        total += num
+
+    print(total)
+
+
+for _ in range(1000):
+    dice()
+
+
